a77a4d1068150e9cc08c69b1b090ce4b8b260a0e
azumyar <yarukizero.dev@gmail.com>
2024-01-16 02:54:05 +0900

    originから最新ファイル同期するバッチ追加

---
+++ origin-sync.bat
@@ -0,0 +1,6 @@
+git clone --filter=blob:none --no-checkout --depth=1 https://github.com/azumyar/illuminate-speech.git .py-recognition-origin
+cd .py-recognition-origin
+git sparse-checkout set src/py-recognition
+git checkout
+cd ..
+robocopy .py-recognition-origin\src\py-recognition py-recognition /MIR
