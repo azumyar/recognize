0415c9a519fcb971cb5182b4d0b3d1ede875d7b2
azumyar <yarukizero.dev@gmail.com>
2024-01-16 21:18:42 +0900

    20240116更新

---
+++ py-recognition/src/__init__.py
@@ -0,0 +1,11 @@
+# https://qiita.com/maebaru/items/f5fecf752c4cf9321a48
+# IPv6だと遅い…？
+# 一度保留 
+#import socket
+#
+#def getAddrInfoWrapper(host, port, family=0, socktype=0, proto=0, flags=0):
+#    # IPv4に限定する
+#    return origGetAddrInfo(host, port, socket.AF_INET, socktype, proto, flags)
+#
+#origGetAddrInfo = socket.getaddrinfo
+#socket.getaddrinfo = getAddrInfoWrapper
