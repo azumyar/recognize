2745646c61a38aba87393870e94c3d3ea1556cbb
azumyar <yarukizero.dev@gmail.com>
2024-02-05 18:52:59 +0900

    再度追加

---
+++ maintenance/origin-sync.bat
@@ -0,0 +1,12 @@
+if exist ".py-recognition-origin" (
+  cd .py-recognition-origin
+  git pull
+  cd ..
+) else (
+  git clone --filter=blob:none --no-checkout --depth=1 https://github.com/azumyar/illuminate-speech.git .py-recognition-origin
+  cd .py-recognition-origin
+  git sparse-checkout set src/py-recognition
+  git checkout
+  cd ..
+)
+robocopy .py-recognition-origin\src\py-recognition ../py-recognition /mir /xd .git
