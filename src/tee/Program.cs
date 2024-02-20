50d49727805a47cc6da774d737f64bd725b5ff3a
azumyar <yarukizero.dev@gmail.com>
2024-02-20 22:04:58 +0900

    tee追加

---
+++ src/tee/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Haru.Kei {
+	static class Tee {
+		static int Main(string[] args) {
+			if(!args.Any()) {
+				Console.WriteLine("usage: tee [OPTION] output-file");
+				return 1;
+			}
+
+			var regex = new Regex(@"(c:\\users\\)([^\\]+)", RegexOptions.IgnoreCase);
+			var file = args.Last();
+			var isMask = (args.Reverse().Skip(1).Where(x => x == "--mask").Any());
+			var line = "";
+			try {
+				using(var stream = new StreamWriter(file, true, Encoding.UTF8)) {
+					while((line = Console.ReadLine()) != null) {
+						Console.WriteLine(line);
+						if(isMask) {
+							line = regex.Replace(line, @"$1***");
+						}
+						stream.WriteLine(line);
+						stream.Flush();
+					}
+				}
+				return 0;
+			}
+			catch(Exception ex) {
+				Console.WriteLine(ex);
+			}
+			return 1;
+		}
+	}
+}
+
+
+
+
