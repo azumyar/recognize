36626765b160780773749b846b05a40a940fe792
azumyar <yarukizero.dev@gmail.com>
2024-12-08 13:22:56 +0900

    CI必要資材およびGithubPages資材

---
+++ src/cs-installer/Const.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Haru.Kei;
+internal static partial class Const {
+	public const string Account = "azumyar";
+	public const string Repository = "recognize";
+	public const string InstallDirectory = ".install";
+	public const string ExtractRootDirectory = "recognize";
+	public static readonly IEnumerable<string> RemoveOldDirectories = new[] { "src" };
+}
