991dee87f8b5c74ed9cceb53576b22db47bfe5bd
azumyar <yarukizero.dev@gmail.com>
2024-12-08 14:03:46 +0900

    アカウントオーナー変更

--- src/cs-installer/Const.cs
+++ src/cs-installer/Const.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace Haru.Kei;
 internal static partial class Const {
-	public const string Account = "azumyar";
+	public const string Account = "HARUKei66494739";
 	public const string Repository = "recognize";
 	public const string InstallDirectory = ".install";
 	public const string ExtractRootDirectory = "recognize";
