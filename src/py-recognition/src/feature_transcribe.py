28fbfa389b8aaac954e6da44bc8706a6c9081f62
azumyar <yarukizero.dev@gmail.com>
2024-12-20 01:59:10 +0900

    出力フォーマットを整理/Logger.printにfileオプション追加

--- src/py-recognition/src/feature_transcribe.py
+++ src/py-recognition/src/feature_transcribe.py
@@ -1,3 +1,4 @@
+import sys
 import audioop
 import wave
 import glob
@@ -16,13 +17,14 @@ def run(
     logger:Logger,
     feature:str) -> None:
 
+    logger.print(f"path,rate,width,channels,transcribe")
     for file in glob.glob(path):
-        logger.print(f"transcribe: {file}")
+        buffer = f"\"{file}\"," 
         try:
             wf = wave.open(file, "rb")
-            logger.print(f"width: {wf.getsampwidth()}")
-            logger.print(f"channels: {wf.getnchannels()}")
-            logger.print(f"rate: {wf.getframerate()}")
+            buffer += f"{wf.getframerate()},"
+            buffer += f"{wf.getsampwidth()},"
+            buffer += f"{wf.getnchannels()},"
 
             d = wf.readframes(-1)
             #if recognition_model.required_sample_rate is not None:
@@ -34,15 +36,18 @@ def run(
                 16000,
                 None)
             ret = recognition_model.transcribe(numpy.frombuffer(d, dtype=numpy.int16).flatten())
-            logger.print(ret.transcribe, console=val.Console.Cyan, reset_console=True)
+            logger.print(buffer, end="")
+            logger.print(f"\"{ret.transcribe}\"")
         except wave.Error as e:
-            logger.print("!!!wave.Error!!!", console=val.Console.Yellow, reset_console=True)
-            logger.print(e, console=val.Console.Yellow, reset_console=True)
+            logger.print(buffer, file=sys.stderr)
+            logger.print("!!!wave.Error!!!", console=val.Console.Yellow, reset_console=True, file=sys.stderr)
+            logger.print(e, console=val.Console.Yellow, reset_console=True, file=sys.stderr)
         except src.recognition.TranscribeException as e:
-            logger.print("!!!TranscribeException!!!", console=val.Console.Yellow, reset_console=True)
-            logger.print(e, console=val.Console.Yellow, reset_console=True)
+            logger.print(buffer, file=sys.stderr)
+            logger.print("!!!TranscribeException!!!", console=val.Console.Yellow, reset_console=True, file=sys.stderr)
+            logger.print(e, console=val.Console.Yellow, reset_console=True, file=sys.stderr)
         except Exception as e:
-            logger.print("!!!Unhandled Exception!!!", console=val.Console.Red, reset_console=True)
-            logger.print(e, console=val.Console.Red, reset_console=True)
-        logger.print(" ") #改行
-    logger.print(f"done.")
+            logger.print(buffer, file=sys.stderr)
+            logger.print("!!!Unhandled Exception!!!", console=val.Console.Red, reset_console=True, file=sys.stderr)
+            logger.print(e, console=val.Console.Red, reset_console=True, file=sys.stderr)
+
