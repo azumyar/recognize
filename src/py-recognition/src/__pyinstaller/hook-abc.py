d1f9b35fc470f4a3a5fd051808e16305d706e41e
azumyar <yarukizero.dev@gmail.com>
2025-10-05 15:48:35 +0900

    exe化に必要なファイルが組み込まれなかった問題を修正

--- src/py-recognition/src/__pyinstaller/hook-abc.py
+++ src/py-recognition/src/__pyinstaller/hook-abc.py
@@ -1,6 +1,7 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs
+from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files
 
-
+datas = []
+datas = collect_data_files("silero_vad")
 
 binaries = []
 binaries += collect_dynamic_libs("openvr")
