dd4d272e3010e0c17631c33d2209ac79e607ac08
azumyar <59293931+azumyar@users.noreply.github.com>
2025-10-17 09:17:32 +0000

    pyinstallerの設定巻き戻し/CIのpythonを3.11に

--- src/py-recognition/src/__pyinstaller/hook-abc.py
+++ src/py-recognition/src/__pyinstaller/hook-abc.py
@@ -1,11 +1,9 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files
 
-hiddenimports = []
-hiddenimports += collect_submodules("scipy")
 
 datas = []
 datas += collect_data_files("silero_vad")
-datas += collect_data_files("scipy")
+
 
 binaries = []
 binaries += collect_dynamic_libs("openvr")
